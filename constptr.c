091e90300b74205d7a9b9acda8108439b1746d0e
anusha <thampuanusha3@gmail.com>
2023-03-23 23:10:21 +0530

    constptr-ptrconst

---
+++ constptr.c
@@ -0,0 +1,49 @@
+// const to ptr //
+
+#include<stdio.h>
+int main()
+{
+	int x=6,y=9;
+
+	int * const ptr=&x;
+
+	*ptr=10;    //values accepted
+
+	//ptr=&x    address not accepted
+
+	printf("%d\n",x);
+
+	int * const ptr1=&y;
+
+	*ptr1=10;
+
+	//ptr1=&y not accepted
+
+	printf("%d\n",y);
+
+}
+
+
+//pointer to const //
+
+#include<stdio.h>
+int main()
+{
+	int x=6,y=9;
+
+	int const * ptr;
+
+	ptr=&x;      //values accpted
+
+	//*ptr=10;   address not accepted
+
+	printf("%d\n",x);
+
+	int const * ptr1;
+
+	ptr1=&y;
+
+	//*ptr1=20;  not accepted
+
+	printf("%d\n",y);
+}
