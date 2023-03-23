54d4881f44893a0b7f51f3faad0cec51ce24642b
anusha <thampuanusha3@gmail.com>
2023-03-24 00:53:09 +0530

    structurepadding

--- structurepadding.c
+++ structurepadding.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#pragma pack(1)
 struct ex1
 {
 	char x;
@@ -22,7 +23,7 @@ int main()
 	struct ex1 a;
 	struct ex2 b;
 	struct ex3 c;
-	printf("size of ex1=%lu",sizeof(a)); //size is 12
-	printf("size of ex2=%lu",sizeof(b)); //size is 8
-	printf("size of ex3=%lu",sizeof(c)); //size is 16
+	printf("size of ex1=%lu",sizeof(a)); //size is 12 after using #pragma size is 7
+	printf("size of ex2=%lu",sizeof(b)); //size is 8 after using #pragma size is 7
+	printf("size of ex3=%lu",sizeof(c)); //size is 16 after using #pragma size is 13
 }
